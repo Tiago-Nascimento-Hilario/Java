d62389a3903dc2d10f98fea41546d47dd5b38590
Tiago <tnhhilario@gmail.com>
2023-02-03 22:59:14 -0300

    Criado metodo para excluir um registro no db

--- aula3/api/src/main/java/com/clinicamedica/api/controller/MedicoController.java
+++ aula3/api/src/main/java/com/clinicamedica/api/controller/MedicoController.java
@@ -1,16 +1,25 @@
 package com.clinicamedica.api.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.clinicamedica.api.medico.DadosAtualizarCadastroMedico;
 import com.clinicamedica.api.medico.DadosCadastroMedico;
+import com.clinicamedica.api.medico.DadosListagemMedico;
 import com.clinicamedica.api.medico.Medico;
 import com.clinicamedica.api.medico.MedicoRepository;
 
 import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/medico")
@@ -21,7 +30,26 @@ public class MedicoController {
 
     @PostMapping
     @Transactional
-    public void cadastrar(@RequestBody DadosCadastroMedico dados){
+    public void cadastrar(@RequestBody  DadosCadastroMedico dados){
         repository.save(new Medico(dados));
     }
+
+    @GetMapping
+    public List<DadosListagemMedico> listar(){
+        return repository.findAll().stream().map(DadosListagemMedico::new).toList();
+    }
+
+    @PutMapping
+    @Transactional
+    public void atualizar(@RequestBody @Valid DadosAtualizarCadastroMedico dados){
+        var medico = repository.getReferenceById(dados.id());
+            medico.atualizarInformacoes(dados);
+    }
+    @DeleteMapping("/{id}")
+    @Transactional
+    public void excluir(@PathVariable Long id){
+        repository.deleteById(id);
+
+    }
+
 }
