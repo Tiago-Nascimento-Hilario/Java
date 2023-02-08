7775ef0905e90aea26633d15c583c219956b8287
Tiago <tnhhilario@gmail.com>
2023-02-08 00:46:45 -0300

    criado ex.  com instrução  For

---
+++ aula4/Estrutura-de-repeticao/For.java
@@ -0,0 +1,8 @@
+public class For {
+    public static void main(String[] args) {
+        //Repetição controlada por contador
+        for(int contador = 0; contador <= 5; contador++){
+            System.out.printf("%d ", contador);
+        }
+    }
+}
