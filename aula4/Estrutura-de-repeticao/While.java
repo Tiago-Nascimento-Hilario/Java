e2af558b2c367f7fcbf678c526b2abedd6edddf4
Tiago <tnhhilario@gmail.com>
2023-02-08 00:48:17 -0300

    criado ex intrução while

---
+++ aula4/Estrutura-de-repeticao/While.java
@@ -0,0 +1,9 @@
+public class While {
+    public static void main(String[] args) {
+        int contador = 0;
+        while(++contador <= 5){
+            System.out.printf("%d ", contador);
+        }
+        System.out.println("FIM!");
+    }
+}
