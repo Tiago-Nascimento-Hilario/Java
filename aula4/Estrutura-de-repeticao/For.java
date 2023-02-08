8521a911acdd953d048aa6bdb5521a8173af371f
Tiago <tnhhilario@gmail.com>
2023-02-08 01:29:57 -0300

    Atualizado logica para verificar números pares e ímpares.

--- aula4/Estrutura-de-repeticao/For.java
+++ aula4/Estrutura-de-repeticao/For.java
@@ -1,8 +1,12 @@
 public class For {
     public static void main(String[] args) {
         //Repetição controlada por contador
-        for(int contador = 0; contador <= 5; contador++){
-            System.out.printf("%d ", contador);
+        for(int contador = 0; contador <= 50; contador++){
+            if(contador % 2 == 0 ){
+                System.out.println(contador + " é par");
+            }else{
+                System.out.println(contador + " é impar");
+            }
         }
     }
 }
